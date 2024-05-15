75d633ed259469d4afcdc35f4770da25d2d0ea2b
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-05-15 18:16:26 +0600

    jwt fixed

--- index.js
+++ index.js
@@ -7,17 +7,33 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-// midlewares
+// Middlewares
 app.use(cors({
-    origin : [
+    origin: [
         "http://localhost:5173",
         "https://assignment-11-1edee.web.app"
     ],
-    credentials : true
+    credentials: true,
 }));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(402).send({ message: 'unauthorized' })
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(402).send({ message: 'unAthorized' })
+        }
+        req.user = decoded;
+        next();
+
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wugjgdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -37,131 +53,147 @@ async function run() {
         const volunteerCollection = database.collection("VolunteerCollection");
         const requestedDatabase = client.db("requestedPostDB");
         const requestedCollection = requestedDatabase.collection("requestedPostCollection");
-        // Connect the client to the server	(optional starting in v4.7)
-        //await client.connect();
 
         // server test
-        app.get("/",(req, res) => {
+        app.get("/", (req, res) => {
             res.send("data coming soon...")
         })
 
+        // jwt token
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "1h"
+            })
+            res.cookie("token", token, {
+                httpOnly: false,
+                secure: true,
+                sameSite: 'none'
+            }).send({ success: true })
+        })
 
         // find first 6 data 
-        app.get("/volunteerneednow", async(req,res)=>{
-            const result = volunteerCollection.find().sort({deadline: 1}).limit(6);
+        app.get("/volunteerneednow", async (req, res) => {
+            const result = volunteerCollection.find().sort({ deadline: 1 }).limit(6);
             const final = await result.toArray()
             res.send(final)
         })
 
         // find all post data 
-        app.get("/volunteerposts", async(req,res)=>{
-            const cursor =  volunteerCollection.find();
+        app.get("/volunteerposts", async (req, res) => {
+            const cursor = volunteerCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
         // find specific post by id 
-        app.get("/volunteerpost/:id", async(req,res)=>{
+        app.get("/volunteerpost/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await volunteerCollection.findOne(query);
             res.send(result);
         })
 
         // add post to database
-        app.post("/addvolunteerpost", async(req,res)=>{
+        app.post("/addvolunteerpost", async (req, res) => {
             const data = req.body;
             const result = await volunteerCollection.insertOne(data);
             res.send(result)
         })
 
         // find my posts
-        app.get("/myposts", async(req,res)=>{
+        app.get("/myposts", verifyToken, async (req, res) => {
             const email = req.query.email;
-            const query = {organizer_email : email };
+            if(email !== req.user.email){
+                return res.status(402).send({message : "forbidden"})
+            }
+            const query = { organizer_email: email };
             const cursor = volunteerCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
 
         // post requested volunteer 
-        app.post("/requestedpost", async(req,res)=>{
+        app.post("/requestedpost", async (req, res) => {
             const data = req.body;
             const result = await requestedCollection.insertOne(data);
             res.send(result);
         })
 
         // get requested volunteer 
-        app.get("/requestedpost",async(req,res)=>{
+        app.get("/requestedpost", verifyToken,async (req, res) => {
             const email = req.query.email;
-            const query = {volunteer_email : email};
+            if(req.user.email !== email){
+                return res.status(403).send({message: "forrrrrrbidden"})
+            }
+            const query = { volunteer_email: email };
             const result = await requestedCollection.find(query).toArray();
-            res.send(result); 
+            res.send(result);
         })
 
         // delete requested post
-        app.delete("/requestedpost/:id", async(req,res)=>{
+        app.delete("/requestedpost/:id", async (req, res) => {
             const id = req.params.id;
-            const query ={_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await requestedCollection.deleteOne(query);
             res.send(result);
         })
 
         // delete post
-        app.delete("/mypost/:id", async(req,res)=>{
+        app.delete("/mypost/:id", async (req, res) => {
             const id = req.params.id;
-            const query ={_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await volunteerCollection.deleteOne(query);
             res.send(result);
         })
 
         // update post
-        app.put("/mypost/update/:id",async(req,res)=>{
+        app.put("/mypost/update/:id", async (req, res) => {
             const id = req.params.id;
             const data = req.body;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const options = { upsert: true };
             const updatedData = {
-                $set : {
-                    thumbnail : data.thumbnail,
-                    post_title : data.post_title,
-                    description : data.description,
-                    category : data.category,
-                    location : data.location,
-                    volunteers_needed : data.volunteers_needed,
-                    organizer_name : data.organizer_name,
-                    organizer_email : data.organizer_email,
-                    deadline : data.deadline
+                $set: {
+                    thumbnail: data.thumbnail,
+                    post_title: data.post_title,
+                    description: data.description,
+                    category: data.category,
+                    location: data.location,
+                    volunteers_needed: data.volunteers_needed,
+                    organizer_name: data.organizer_name,
+                    organizer_email: data.organizer_email,
+                    deadline: data.deadline
                 }
             }
-            const result = await volunteerCollection.updateOne(query,updatedData,options)
+            const result = await volunteerCollection.updateOne(query, updatedData, options)
             res.send(result)
         })
 
+        // update a posts volunterr count  decrease
+        app.patch("/volunteerpost/decrease/:id", async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const updatedDoc = {
+                $inc: { volunteers_needed: -1 }
+            }
+            const result = await volunteerCollection.updateOne(query,updatedDoc)
+            res.send(result);
+        })
+
+        
         // search functionality
-        app.get("/mypost/search",async(req,res)=>{
+        app.get("/mypost/search", async (req, res) => {
             const title = req.query.text;
-            console.log(title);
-            const query = {post_title : {$regex : title}} 
+            const query = { post_title: { $regex: title } }
             const cursor = volunteerCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        // JWT token
-        app.post("/jwt", async(req,res)=>{
-            const user = req.body;
-            const token = jwt.sign(user,"secret",{
-                expiresIn : "1h"
-            })
-            res.cookie("token",token,{
-                httpOnly: false,
-                secure: true,
-                sameSite : 'none'
-            }).send({success : true})
-        })
-        // Send a ping to confirm a successful connection
 
+
+        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
@@ -170,7 +202,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
     console.log(`server Running On Port ${port}`);
